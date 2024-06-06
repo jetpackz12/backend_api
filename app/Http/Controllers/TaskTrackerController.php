a7ac9cc801d0dce1bc4ba61c81b2adc8e92ed375
jett <jetpackz12@gmail.com>
2024-06-06 04:23:38 +0000

    feat/PaginationEditShowData

--- app/Http/Controllers/TaskTrackerController.php
+++ app/Http/Controllers/TaskTrackerController.php
@@ -15,7 +15,7 @@ class TaskTrackerController extends Controller
     public function index()
     {
         $render_data = [
-            'tasks' => DB::table('task_trackers')->get()
+            'tasks' => DB::table('task_trackers')->orderBy('id', 'desc')->paginate(5)
         ];
 
         return response()->json($render_data);
@@ -29,7 +29,7 @@ class TaskTrackerController extends Controller
         try {
 
             $task = DB::table('task_trackers')->where('task', '=', $request->task)->first();
-    
+
             if ($task) return response()->json($this->renderMessage(0, 'This task is already exist!'), Response::HTTP_BAD_REQUEST);
 
             $form_data = $request->validate([
@@ -37,13 +37,41 @@ class TaskTrackerController extends Controller
                 'date_time' => 'required',
                 'status' => 'required',
             ]);
-    
+
             $task = TaskTracker::create($form_data);
-    
+
             if ($task) return response()->json($this->renderMessage(1, 'Task added success!'), Response::HTTP_OK);
 
         } catch (\Throwable $th) {
 
+            return response()->json($this->renderMessage(0, 'An error occurred: ' . $th->getMessage()), Response::HTTP_INTERNAL_SERVER_ERROR);
+
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Request $request)
+    {
+        $search = $request->input('search');
+
+        $task = DB::table('task_trackers')->where('task', 'LIKE', '%'.$search.'%')->orWhere('date_time', 'LIKE', '%'.$search.'%')->orderBy('id', 'desc')->paginate(5);
+
+        return response()->json($task);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(TaskTracker $taskTracker)
+    {
+        try {
+
+            return response()->json($taskTracker, Response::HTTP_OK);
+
+        } catch (\Throwable $th) {
+            
             return response()->json($this->renderMessage(0, 'An error occurred: ' . $th->getMessage()), Response::HTTP_INTERNAL_SERVER_ERROR);
 
         }
@@ -57,18 +85,40 @@ class TaskTrackerController extends Controller
         try {
 
             $task = DB::table('task_trackers')->where('task', '=', $request->task)->where('id', '!=', $taskTracker->id)->first();
-    
-            if ($task) return response()->json($this->renderMessage(0, 'This task is already exist!'), Response::HTTP_BAD_REQUEST);
+
+            if ($task) return response()->json($this->renderMessage(0, 'This task is already exist!', $taskTracker), Response::HTTP_BAD_REQUEST);
 
             $form_data = $request->validate([
                 'task' => 'required',
                 'date_time' => 'required',
                 'status' => 'required',
             ]);
-    
+
             $task = $taskTracker->update($form_data);
-    
-            if ($task) return response()->json($this->renderMessage(1, 'Task updated success!'), Response::HTTP_OK);
+
+            if ($task) return response()->json($this->renderMessage(1, 'Task updated success!', $taskTracker), Response::HTTP_OK);
+
+        } catch (\Throwable $th) {
+
+            return response()->json($this->renderMessage(0, 'An error occurred: ' . $th->getMessage()), Response::HTTP_INTERNAL_SERVER_ERROR);
+
+        }
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function updateStatus(Request $request, TaskTracker $taskTracker)
+    {
+        try {
+
+            $form_data = $request->validate([
+                'status' => 'required',
+            ]);
+
+            $task = $taskTracker->update($form_data);
+
+            if ($task) return response()->json($this->renderMessage(1, 'Task updated success!', $taskTracker), Response::HTTP_OK);
 
         } catch (\Throwable $th) {
 
@@ -85,21 +135,21 @@ class TaskTrackerController extends Controller
         try {
 
             $task = $taskTracker->delete();
-    
+
             if ($task) return response()->json($this->renderMessage(1, 'Task deleted success!'), Response::HTTP_OK);
 
         } catch (\Throwable $th) {
 
             return response()->json($this->renderMessage(0, 'An error occurred: ' . $th->getMessage()), Response::HTTP_INTERNAL_SERVER_ERROR);
-
         }
     }
 
-    public function renderMessage($status, $message)
+    public function renderMessage($status, $message, $res_data = [])
     {
         return [
             'status' => $status,
-            'message' => $message
+            'message' => $message,
+            'res_data' => $res_data
         ];
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -10,8 +10,17 @@ Route::get('/v1/tasks', [TaskTrackerController::class, 'index']);
 // Insert new task
 Route::post('/v1/tasks/store', [TaskTrackerController::class, 'store']);
 
+// Search task
+Route::get('/v1/tasks/show', [TaskTrackerController::class, 'show']);
+
+// Edit task
+Route::get('/v1/tasks/edit/{taskTracker}', [TaskTrackerController::class, 'edit']);
+
 // Updating task
 Route::post('/v1/tasks/update/{taskTracker}', [TaskTrackerController::class, 'update']);
 
+// Updating task status
+Route::post('/v1/tasks/updateStatus/{taskTracker}', [TaskTrackerController::class, 'updateStatus']);
+
 // destroying task
-Route::post('/v1/tasks/destroy/{taskTracker}', [TaskTrackerController::class, 'destroy']);
+Route::delete('/v1/tasks/destroy/{taskTracker}', [TaskTrackerController::class, 'destroy']);
